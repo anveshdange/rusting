26c97b61fd9ce054e2baafe670aa233aeece2705
anveshdange <dangeanv@gmail.com>
2023-10-14 04:54:14 +0530

    first project

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,25 @@
+// Modular imports 
 use std::io;
-
+use rand::Rng; // Bringing range crait to scope
+use std::cmp::Ordering;
 fn main() {
-    println!("Guess the Number!"); 
-    println!("please give your guess: ");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed: {}", guess);
+    println!("Guess the number!!");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The number is: {}", secret_number);
+    loop{
+        println!("Please input your guess: ");
+        let mut guess = String::new() ;
+
+        io::stdin()
+            .read_line(&mut guess) 
+            .expect("Failed to read line");
+        let guess: u32 = guess.trim().parse().expect("Please type a number");
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {println!("You win!");break;},
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("Too small"),
+        }
+    }
+    
 }
